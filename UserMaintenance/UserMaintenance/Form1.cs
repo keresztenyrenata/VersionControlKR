362ad70f450b5749b2b9474001ab14b97b1abc9f
Renáta Keresztény <keresztenyrenata@outlook.com>
2019-12-01 11:50:42 +0100

    save

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace UserMaintenance
             
             //lblFirstName.Text = Resource1.FirstName;
             btnAdd.Text = Resource1.Add;
+            btnSave.Text = Resource1.Felirat;
 
             listUsers.DataSource = users;
             listUsers.ValueMember = "ID";
@@ -38,6 +40,34 @@ namespace UserMaintenance
                 FullName = txtLastName.Text
             };
             users.Add(u);
-        } 
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            sfd.InitialDirectory = Application.StartupPath;
+            sfd.Filter = "Comma Seperated Values (*.csv)|*.csv";
+            sfd.DefaultExt = "csv";
+            sfd.AddExtension = true;
+
+            if (sfd.ShowDialog() != DialogResult.OK) return;
+
+            using (StreamWriter sw = new StreamWriter(sfd.FileName, false, Encoding.UTF8))
+            {
+
+                sw.Write("ID:");
+                sw.Write(";");
+                sw.Write("Fullname:");
+                sw.WriteLine();
+
+                foreach (User u in listUsers.Items) sw.WriteLine(string.Format("{0};{1}", u.ID, u.FullName));
+
+            }
+
+            MessageBox.Show("A fájlba írás megtörtént");
+
+            Application.Exit();
+        }
     }
 }
