660a4e78c8d7a1ead4bb06828add4fa834070569
Renáta Keresztény <keresztenyrenata@outlook.com>
2019-12-01 11:41:46 +0100

    fullname

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -19,8 +19,9 @@ namespace UserMaintenance
         {
             InitializeComponent();
 
-            lblLastName.Text = Resource1.LastName;
-            lblFirstName.Text = Resource1.FirstName;
+            lblLastName.Text = Resource1.FullName;
+            
+            //lblFirstName.Text = Resource1.FirstName;
             btnAdd.Text = Resource1.Add;
 
             listUsers.DataSource = users;
@@ -32,8 +33,9 @@ namespace UserMaintenance
         {
             var u = new User()
             {
-                LastName = txtLastName.Text,
-                FirstName = txtFirstName.Text
+                //LastName = txtLastName.Text,
+                //FirstName = txtFirstName.Text,
+                FullName = txtLastName.Text
             };
             users.Add(u);
         } 
